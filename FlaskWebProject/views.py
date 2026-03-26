6fca156bce68f0ba6902bd8cdfcfa7b3b0ef150b
LucasWuChevron <lucaswu@chevron.com>
2026-03-26 13:26:47 -0700

    Add cache management for MSAL tokens
    Implement cache loading and saving for MSAL tokens.

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -111,13 +111,16 @@ def logout():
     return redirect(url_for('login'))
 
 def _load_cache():
-    # TODO: Load the cache from `msal`, if it exists
-    cache = None
-    return cache
+ # TODO: Load the cache from `msal`, if it exists
+ cache = msal.SerializableTokenCache()
+ if session.get('token_cache'):
+ cache.deserialize(session['token_cache'])
+ return cache
 
 def _save_cache(cache):
-    # TODO: Save the cache, if it has changed
-    pass
+ # TODO: Save the cache, if it has changed
+ if cache.has_state_changed:
+ session['token_cache'] = cache.serialize()
 
 def _build_msal_app(cache=None, authority=None):
  # TODO: Return a ConfidentialClientApplication
